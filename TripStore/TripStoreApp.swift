f164e9e80d53c068e38b66ae26ef977eb5d195e8
Emad Ayad <emadayad228@gmail.com>
2026-09-15 12:45:35 +0300

    initalize appt

---
+++ TripStore/TripStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripStoreApp.swift
+//  TripStore
+//
+//  Created by TaqieAllah on 15/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct TripStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
